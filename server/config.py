15221bc6430051ab758be09332974568a35a542d
ajin3830 <ajin3830@gmail.com>
2023-03-28 18:09:50 -0400

    added routes to app.py, modified config.py

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_cors import CORS
