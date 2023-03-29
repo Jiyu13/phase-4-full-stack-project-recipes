42152e6979bdce4ba8c51b0e13bf02923b775473
Ziru Zou <jiyucoding@yahoo.com>
2023-03-29 18:22:22 -0400

    add styling to logo and navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,38 +3,28 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav className="NavBar">
-      <div className="">
-        <Link to='/'>RecipEase</Link>
+        {/* <Link to='/'>RecipEase</Link> */}
+      <h1 className="logo">RecipEase</h1>
+      <div className='navigate-to'>
+        
+            <Link to="/signup">
+            <button className="NavBarButton">Sign Up</button>
+            </Link>
+         
+            <Link to="/login">
+            <button className="NavBarButton">Login</button>
+            </Link>
+          
+            <Link to="/logout">
+            <button className="NavBarButton">Logout</button>
+            </Link>
+          
+            <Link to="/filter">
+            <button className="NavBarButton">Filter</button>
+            </Link>
+         
       </div>
-      <ul>
-        {/* <li>
-          <Link to="/">
-            <button className="NavBarButton">Home</button>
-          </Link>
-        </li> */}
-
-        <li>
-          <Link to="/signup">
-          <button className="NavBarButton">Sign Up</button>
-          </Link>
-        </li>
-
-        <li>
-          <Link to="/login">
-          <button className="NavBarButton">Login</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/logout">
-          <button className="NavBarButton">Logout</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/filter">
-          <button className="NavBarButton">Filter</button>
-          </Link>
-        </li>
-      </ul>
+      
     </nav>
   );
 }
